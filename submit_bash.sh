0db89fa3869448c49f173749ff9519dcd7b0e942
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-15 12:12:51 +1000

    Refactor submit_bash.sh to streamline model training and testing commands; removed commented-out lines and enabled testing for the FNO model with specified parameters.

--- submit_bash.sh
+++ submit_bash.sh
@@ -21,14 +21,7 @@ source /scratch3/wan410/venvs/testing/bin/activate
 
 ## Train the model
 # (CUDA_VISIBLE_DEVICES=0 python3 train_customized.py --dataset='ns2d_pda' --use_writer --model='wavelet_transformer' --lr_method='cossin' --T_in=7)
-(CUDA_VISIBLE_DEVICES=0 python3 train_customized.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7)
-# (CUDA_VISIBLE_DEVICES=0 python3 train_customized.py --use_writer --dataset='sw2d_pda' --model='wavelet_transformer' --lr_method='cossin' --T_in=10)
-# (CUDA_VISIBLE_DEVICES=0 python3 train_flow_matching_framework.py --use_writer --dataset='ns2d_pda' --model='FNO' --lr_method='cossin')
-# (CUDA_VISIBLE_DEVICES=0 python3 train_flow_matching_framework.py --use_writer --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin')
-
-
-## use writer to save the model
-# (CUDA_VISIBLE_DEVICES=0 python3 train_customized.py  --use_writer --dataset='ns2d_pda' --model='FNO' --lr_method='cossin')
+# (CUDA_VISIBLE_DEVICES=0 python3 train_customized.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7)
 
 ## resume training
 # CUDA_VISIBLE_DEVICES=0 python3 train_customized.py  --use_writer --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin' --resume_path=True
@@ -36,13 +29,4 @@ source /scratch3/wan410/venvs/testing/bin/activate
 
 ## Test the model
 # CUDA_VISIBLE_DEVICES=0 python3 test_customized.py --dataset='sw2d_pda' --model='FNO'
-# CUDA_VISIBLE_DEVICES=0 python3 test_customized.py --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin' --T_in=7
-# CUDA_VISIBLE_DEVICES=0 python3 test_customized.py --dataset='ns2d_pda' --model='FNO' --T_in=7
-# CUDA_VISIBLE_DEVICES=0 python3 test_customized.py --dataset='sw2d_pda' --model='wavelet_transformer' --lr_method='cossin' --T_in=10
-
-## Test resolution invariant
-# (CUDA_VISIBLE_DEVICES=0 python3 test_customized_resolution_invariant.py --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin')
-
-##  Test different loss functions
-# (CUDA_VISIBLE_DEVICES=0 python3 train_customized_loss_ablation.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --loss='nll' --resume_path=True)
-# (CUDA_VISIBLE_DEVICES=0 python3 train_customized_loss_ablation.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --loss='fourier' --resume_path=1)
+CUDA_VISIBLE_DEVICES=0 python3 test_customized.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7
