5ddd13cce2514fa0d7fb2282a89250a8346d79ac
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-18 14:18:51 +1000

    new env

--- submit_bash.sh
+++ submit_bash.sh
@@ -15,7 +15,8 @@ module load pytorch/2.5.1-py312-cu122-mpi
 # module load pytorch/2.1.0-py312-cu122-mpi
 module load tensorflow/2.16.1-pip-py312-cuda122
 module load parallel python
-source /scratch3/wan410/venvs/testing/bin/activate
+# source /scratch3/wan410/venvs/testing/bin/activate
+source $HOME/.venvs/pytorch/bin/activate
 
 
 
