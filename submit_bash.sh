7c89f76dabfb1122f499a22ca34c02c4ede302de
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-21 10:44:27 +1000

    test

--- submit_bash.sh
+++ submit_bash.sh
@@ -30,8 +30,8 @@ source $HOME/.venvs/pytorch/bin/activate
 
 ## Test the model
 # CUDA_VISIBLE_DEVICES=0 python3 test_AR_NO.py --dataset='sw2d_pda' --model='FNO'
-CUDA_VISIBLE_DEVICES=0 python3 test_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7
+# CUDA_VISIBLE_DEVICES=0 python3 test_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7
 
 
 # Train the diffusion model
-# CUDA_VISIBLE_DEVICES=0 python3 train_diffusion_NO.py --dataset='ns2d_pda' --model_name='FNO' --T_in=7
+CUDA_VISIBLE_DEVICES=0 python3 train_diffusion_NO.py --dataset='ns2d_pda' --model_name='FNO' --T_in=7
