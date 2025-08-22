090217664f59b3464888c1bf3172d957e9f23dd1
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-22 10:17:40 +1000

    test gpus

--- run_lightning_multi_gpu.sh
+++ run_lightning_multi_gpu.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --account=OD-230881
 #SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
 
