9ef9046774c17998acc46cad10e7523c124dc4d3
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-25 15:07:27 +1000

    tes

--- utils_plot.py
+++ utils_plot.py
@@ -1232,14 +1232,14 @@ if __name__ == '__main__':
     ntrain = 7000 if args.dataset == 'sw2d_pda' else 5200
     comment = args.comment + '{}_{}_ntrain{}'.format(args.model, args.dataset, ntrain)
     log_path = args.log_path + comment if len(args.log_path) > 0 else './logs/' + comment
-    # FNO test data
-    # pred_data = torch.load(f'{log_path}/test_data_prediction.pth', map_location=device)
-    # plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name='FNO', log_path=log_path)
+    # FNO/Wavelet test data
+    pred_data = torch.load(f'{log_path}/test_data_prediction.pth', map_location=device)
+    plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name=args.model, log_path=log_path)
     
     
     # FNO-Diffusion test data
-    pred_data = torch.load(f'{log_path}/test_data_diffusion_prediction.pth', map_location=device)
-    plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name='FNO-Diffusion', log_path=log_path)
+    # pred_data = torch.load(f'{log_path}/test_data_diffusion_prediction.pth', map_location=device)
+    # plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name='FNO-Diffusion', log_path=log_path)
 
     
     
