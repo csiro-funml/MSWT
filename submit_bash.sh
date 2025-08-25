c8f5915459768c315cb1468bceaa3119eae8e50a
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-25 14:31:00 +1000

    train FNO for a longer epoch

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=00:03:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -22,7 +22,7 @@ source $HOME/.venvs/pytorch/bin/activate
 
 ## Train the model
 # (CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --use_writer --model='FNO' --lr_method='cossin' --T_in=7)
-# (CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7)
+CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=20000
 
 ## resume training
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py  --use_writer --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin' --resume_path=True
@@ -39,4 +39,4 @@ source $HOME/.venvs/pytorch/bin/activate
 
 
 # Utile plotting in the server
-CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='FNO' 
+# CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='FNO' 
