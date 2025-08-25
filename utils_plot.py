62c7540e2e88f76aca8b69aacc3fd752b50751eb
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-25 13:50:50 +1000

    t

--- utils_plot.py
+++ utils_plot.py
@@ -1236,7 +1236,7 @@ if __name__ == '__main__':
     
     
     # FNO-Diffusion test data
-    pred_data = torch.load(f'{log_path}/test_data_diffusion.pth', map_location=device)
+    pred_data = torch.load(f'{log_path}/test_data_diffusion_prediction.pth', map_location=device)
     plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name='FNO-Diffusion', log_path=log_path)
 
     
