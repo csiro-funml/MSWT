fa3f1036613efc35664a35c74810d67ae6152b5e
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-27 13:41:30 +1000

    train Wavelet

--- submit_bash.sh
+++ submit_bash.sh
@@ -21,11 +21,10 @@ source $HOME/.venvs/pytorch/bin/activate
 
 
 ## Train the model
-# CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO_better_scheduler.py --dataset='ns2d_pda' --use_writer --model='FNO' --lr_method='cossin' --T_in=7 --epochs=10000
-CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='HFS' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
+# CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='HFS' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
-# CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='wavelet_transformer' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
-# CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=2000
+CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='wavelet_transformer' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
+
 
 ## resume training
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py  --use_writer --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin' --resume_path=True
