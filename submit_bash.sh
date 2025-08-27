95ad21608e9b24569e6c818e499ee87bb23e5f24
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-08-27 12:05:04 +1000

    test

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=20:00:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -23,7 +23,7 @@ source $HOME/.venvs/pytorch/bin/activate
 ## Train the model
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO_better_scheduler.py --dataset='ns2d_pda' --use_writer --model='FNO' --lr_method='cossin' --T_in=7 --epochs=10000
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='HFS' --use_write --lr_method='cossin' --T_in=7 --epochs=3000 --opt='lion'
-CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=3000 --opt='lion'
+CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=3000 --opt='adam'
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='wavelet_transformer' --lr_method='cossin' --T_in=7 --epochs=3000 --opt='lion'
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=2000
 
