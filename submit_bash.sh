2806d52bfdacce11002360a4c895a63fbe429e88
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-01 18:20:28 +1000

    TEST PDEREFINER

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=15:30:00
+#SBATCH --time=00:05:00
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -45,7 +45,8 @@ source $HOME/.venvs/pytorch/bin/activate
 
 
 # Train the PDERefiner model
-CUDA_VISIBLE_DEVICES=0 python3 train_pderefiner.py --dataset='ns2d_pda' --model='PDERefiner' --T_in=7 --T_ar=1 --epochs=3000  --use_writer
+# CUDA_VISIBLE_DEVICES=0 python3 train_pderefiner.py --dataset='ns2d_pda' --model='PDERefiner' --T_in=7 --T_ar=1 --epochs=3000  --use_writer
+CUDA_VISIBLE_DEVICES=0 python3 test_pderefiner.py --dataset='ns2d_pda' --model='PDERefiner' --T_in=7 --T_ar=1 --batch_size=128
 
 # Utile plotting in the server
 # CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='HFS' 
