406237523cd018ae852a70ae56341b7033251275
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-01 16:59:38 +1000

    test

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=00:05:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -45,9 +45,9 @@ source $HOME/.venvs/pytorch/bin/activate
 
 
 # Train the PDERefiner model
-# CUDA_VISIBLE_DEVICES=0 python3 train_pderefiner.py --dataset='ns2d_pda' --model='PDERefiner' --use_writer --T_in=7 --T_ar=1 --epochs=3000
+CUDA_VISIBLE_DEVICES=0 python3 train_pderefiner.py --dataset='ns2d_pda' --model='PDERefiner' --T_in=7 --T_ar=1 --epochs=3000
 
 # Utile plotting in the server
 # CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='HFS' 
-CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='FNO' 
+# CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='FNO' 
 # CUDA_VISIBLE_DEVICES=0 python3 utils_plot.py --dataset='ns2d_pda' --model='wavelet_transformer' 
