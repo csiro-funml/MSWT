dbc7f2fa89ed290be8a4e6947bbd4d5f024aabef
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-02 17:12:08 +1000

    mv files

--- pdearena/sw2d_pda/split_script.py
+++ pdearena/sw2d_pda/split_script.py
@@ -7,7 +7,7 @@ val_path = os.path.join(folder_path, 'val')
 
 # move the last 100 files (data_6899.hdf5 to data_6999.hdf5) from train to val and reindex from the 0 to 99.hdf5
 for i in range(100):
-    os.rename(os.path.join(train_path, f'data_{6899 + i}.hdf5'), os.path.join(val_path, f'data_{i}.hdf5'))
+    os.system(f'mv {os.path.join(train_path, f"data_{6899 + i}.hdf5")} {os.path.join(val_path, f"data_{i}.hdf5")}')
     print(f'moved data_{6899 + i}.hdf5 to val folder')
 
 
