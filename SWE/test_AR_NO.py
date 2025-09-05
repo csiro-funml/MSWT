94f7dd750ceda95ddefe6151bfb874cf268f810d
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-05 14:39:33 +1000

    quick testing

--- SWE/test_AR_NO.py
+++ SWE/test_AR_NO.py
@@ -419,11 +419,11 @@ def no_postprocessing_pred_save_data(args):
 if __name__ == '__main__':
     
     #### 1. predict and save the data
-    model, test_loader, log_path = load_data_model(just_load_path=False)
-    save_data = predict_and_save(model, test_loader, save=True, log_path=log_path, max_steps=30)
+    model, test_loader, log_path = load_data_model(just_load_path=True)
+    # save_data = predict_and_save(model, test_loader, save=True, log_path=log_path, max_steps=30)
     
     #### 2. load the save_data
-    # save_data = torch.load(f'{log_path}/test_data_prediction.pth', map_location=device)
+    save_data = torch.load(f'{log_path}/test_data_prediction.pth', map_location=device)
     
     #### 3. compute different types of metrics
     compute_evalutation_metrics(save_data, model_name=args.model, log_path=log_path)
