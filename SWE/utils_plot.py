6b724af9cb7130007e12a35113d1ac5e1da7098e
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-05 12:46:24 +1000

    fix bugging in plotting

--- SWE/utils_plot.py
+++ SWE/utils_plot.py
@@ -1129,15 +1129,13 @@ def plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name
     # make them symmetrical around zero
     vmax = max(abs(vmin), abs(vmax))
     vmin = -vmax
-
     # cuz I want to text the different baseliens, I need to fix the threshol for the error (5% of the output)
     error_vmin = 0.05*vmin
     error_vmax = 0.05*vmax
-        
     
     cmap = 'RdBu_r'
     total_steps_to_plot = 3 
-    fig, axes = plt.subplots(2*total_steps_to_plot, 2, figsize=(9, 5*total_steps_to_plot)) if model_name == 'FNO' else plt.subplots(2*total_steps_to_plot, 2, figsize=(9, 4*total_steps_to_plot))
+    fig, axes = plt.subplots(2*total_steps_to_plot, 2, figsize=(12, 5*total_steps_to_plot)) if model_name == 'FNO' else plt.subplots(2*total_steps_to_plot, 2, figsize=(12, 4*total_steps_to_plot))
     
 
     for row_idx, time_idx in enumerate(range(0, pred.shape[-1], pred.shape[-1]//total_steps_to_plot)):
@@ -1156,17 +1154,17 @@ def plot_prediction_gt_abserror(pred_data, sample_id=0, channel_id=0, model_name
     
         # DRAW THE SECOND COLUMN
         # axes[0, 1] to axes[1, 1] is the prediction and the abs error at the first time step
-        cm0 = axes[2*row_idx, 1].imshow(pred[..., 0], vmin=vmin, vmax=vmax, cmap=cmap)
+        cm0 = axes[2*row_idx, 1].imshow(pred[..., time_idx], vmin=vmin, vmax=vmax, cmap=cmap)
         axes[2*row_idx, 1].set_ylabel('Pred T'+f'{time_idx+1}')
         axes[2*row_idx, 1].set_xticks([])
         axes[2*row_idx, 1].set_yticks([])
-        fig.colorbar(cm0, ax=axes[2*row_idx, 1], location='right', anchor=(0, 0.5), shrink=0.5) # add colorbar
+        fig.colorbar(cm0, ax=axes[2*row_idx, 1], location='right', anchor=(0, 0.5), shrink=0.6) # add colorbar
         
-        cm1 = axes[2*row_idx+1, 1].imshow(error[..., 0], cmap=cmap, vmin=error_vmin, vmax=error_vmax)
+        cm1 = axes[2*row_idx+1, 1].imshow(error[..., time_idx], cmap=cmap, vmin=error_vmin, vmax=error_vmax)
         axes[2*row_idx+1, 1].set_ylabel('Error T'+f'{time_idx+1}')
         axes[2*row_idx+1, 1].set_xticks([])
         axes[2*row_idx+1, 1].set_yticks([])
-        fig.colorbar(cm1, ax=axes[2*row_idx+1, 1], location='right', anchor=(0, 0.5), shrink=0.5)
+        fig.colorbar(cm1, ax=axes[2*row_idx+1, 1], location='right', anchor=(0, 0.5), shrink=0.6)
     
     # tight layout
     fig.tight_layout()
