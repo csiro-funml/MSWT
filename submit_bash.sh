e2f828da0a05196c55759dc13d8135eb40a5b47e
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-05 14:16:25 +1000

    save k_low/high

--- submit_bash.sh
+++ submit_bash.sh
@@ -61,9 +61,9 @@ source $HOME/.venvs/pytorch/bin/activate
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/train_AR_NO.py --dataset='sw2d_pda' --model='HFS' --lr_method='cossin' --T_in=7 --epochs=2000 --use_writer --resume_path=True --batch_size=128
 
 # Test the model
-# CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='FNO' --T_in=7 --epochs=2000
+CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='FNO' --T_in=7 --epochs=2000
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='wavelet_transformer' --T_in=7 --epochs=2000
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='HFS' --T_in=7 --epochs=2000
 
-CUDA_VISIBLE_DEVICES=0 python3 SWE/utils_plot.py --dataset='sw2d_pda' --model='FNO' 
+# CUDA_VISIBLE_DEVICES=0 python3 SWE/utils_plot.py --dataset='sw2d_pda' --model='FNO' 
 ################################################################ SWE #################################################################
