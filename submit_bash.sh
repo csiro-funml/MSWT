5a5f3c44c0ee6cf07e1e134d73a2c652d5f1148a
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-16 16:19:02 +1000

    run bigger patch size

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:30:00
+#SBATCH --time=15:00:00
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -23,7 +23,7 @@ source $HOME/.venvs/pytorch/bin/activate
 
 ################################################################ NSE #################################################################
 ## Train the model
-CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_pda' --model='FFormer' --lr_method='cossin' --T_in=7 --epochs=3000 --patch_size=8
+CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_pda' --model='FFormer' --use_write --lr_method='cossin' --T_in=7 --epochs=3000 --patch_size=8
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='HFS' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='wavelet_transformer' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
