932edab2ca976d6344e99e80b784c5aefc411d23
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-16 17:55:16 +1000

    debugging

--- SWE/test_AR_NO.py
+++ SWE/test_AR_NO.py
@@ -226,8 +226,8 @@ def predict_and_save(model, test_loader, save=False, log_path=None, max_steps=No
 
                 # preint 
             for t in range(0, yy.shape[-2], args.T_bundle):
-                if t > 30 and max_steps is not None:
-                    break
+                # if t > 30 and max_steps is not None:
+                    # break
                 im = model(xx)
                 if t == 0:
                     pred = im
