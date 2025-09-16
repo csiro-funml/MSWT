8604bb2851cdd3059cdbf8fd8453e4d13c903709
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-16 17:55:36 +1000

    debugging

--- submit_bash.sh
+++ submit_bash.sh
@@ -70,7 +70,7 @@ source $HOME/.venvs/pytorch/bin/activate
 # Test the model
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='FNO' --T_in=7 --epochs=2000
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='wavelet_transformer' --T_in=7 --epochs=2000
-# CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='HFS' --T_in=7 --epochs=2000
+CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='HFS' --T_in=7 --epochs=2000
 
 CUDA_VISIBLE_DEVICES=0 python3 SWE/utils_plot.py --dataset='sw2d_pda' --model='HFS' 
 ################################################################ SWE #################################################################
