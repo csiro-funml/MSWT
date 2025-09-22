490b00fd856bb0045ea7b5f0e5da238d74f101e7
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-22 14:49:05 +1000

    run fno ns2d torch-cfd

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:10:00
+#SBATCH --time=15:10:00
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -25,7 +25,8 @@ source $HOME/.venvs/pytorch/bin/activate
 ## Train the model
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_pda' --model='wavelet_transformer_skip' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_fno_1e-3' --model='HFS' --lr_method='cossin' --T_in=7 --epochs=3000
-CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_torchcf_re1000' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=3000
+CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_torchcf_re1000' --model='FNO' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
+
 
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='HFS' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
 # CUDA_VISIBLE_DEVICES=0 python3 train_AR_NO.py --dataset='ns2d_pda' --model='FNO' --use_write --lr_method='cossin' --T_in=7 --epochs=3000
