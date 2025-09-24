b194d56befa5079b41c9285cd0d426dc6d743ac5
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-24 12:17:54 +1000

    solve package

--- submit_bash.sh
+++ submit_bash.sh
@@ -12,10 +12,11 @@
 
 
 # module load numpy/2.0.0-py312
-module load pytorch/2.5.1-py312-cu122-mpi
+# module load pytorch/2.5.1-py312-cu122-mpi
 # module load pytorch/2.5.1-py312-cu124-mpi-sota
 # module load pytorch/2.1.0-py312-cu122-mpi
 module load tensorflow/2.16.1-pip-py312-cuda122
+module load mmcv/
 module load parallel python
 # source /scratch3/wan410/venvs/testing/bin/activate
 source $HOME/.venvs/pytorch/bin/activate
