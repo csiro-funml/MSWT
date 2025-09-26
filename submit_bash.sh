93893d40e8e9f14ab36916cc977c1a52b3f48646
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-09-26 10:05:19 +1000

    test running WaveletV2 on SWE

--- submit_bash.sh
+++ submit_bash.sh
@@ -77,9 +77,9 @@ source $HOME/.venvs/pytorch/bin/activate
 # Plotting
 
 
-CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000' --model='FNO'
-CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000' --model='HFS'
-CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000' --model='WaveletTransV2'
+# CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000' --model='FNO'
+# CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000' --model='HFS'
+# CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000' --model='WaveletTransV2'
 #################################################################################################################################
 
 
@@ -89,6 +89,7 @@ CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_torchcf_re1000'
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/train_AR_NO.py --dataset='sw2d_pda' --model='FNO' --lr_method='cossin' --T_in=7 --use_writer --resume_path=True --epochs=2000 --batch_size=128
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/train_AR_NO.py --dataset='sw2d_pda' --model='wavelet_transformer' --lr_method='cossin' --T_in=7 --use_writer --resume_path=True --epochs=2000 --batch_size=128
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/train_AR_NO.py --dataset='sw2d_pda' --model='HFS' --lr_method='cossin' --T_in=7 --epochs=2000 --use_writer --resume_path=True --batch_size=128
+CUDA_VISIBLE_DEVICES=0 python3 SWE/train_AR_NO.py --dataset='sw2d_pda' --model='WaveletTransV2' --lr_method='cossin' --T_in=7 --epochs=2000 --batch_size=128
 
 # Test the model
 # CUDA_VISIBLE_DEVICES=0 python3 SWE/test_AR_NO.py --dataset='sw2d_pda' --model='FNO' --T_in=7 --epochs=2000
