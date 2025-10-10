aa3e92c308b096f697e2fa4755626210a91c63c6
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-10 11:38:42 +1100

    Remove GPU resource allocation from submit_bash.sh to streamline job submission parameters.

--- submit_bash.sh
+++ submit_bash.sh
@@ -5,7 +5,6 @@
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --account=OD-230881
 #SBATCH --cpus-per-task=9        # cpu-cores per task (>1 if multi-threaded tasks)
