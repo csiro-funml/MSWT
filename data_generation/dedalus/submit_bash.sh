3dd8d4ae81b0b92c9ee5b7c93ba12d8f55612a34
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-10 11:04:35 +1100

    Add new submit_bash.sh script for Dedalus job submission
    This script sets up the SLURM job parameters, loads the necessary PyTorch module, activates the Python virtual environment, and runs the preprocessing script for Dedalus data generation.

---
+++ data_generation/dedalus/submit_bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH --time=00:10:00
+
+#SBATCH --mem=256gb
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=OD-230881
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+
+
+module load pytorch/2.5.1-py312-cu122-mpi
+source $HOME/.venvs/pytorch/bin/activate
+
+python3 preprocess.py
