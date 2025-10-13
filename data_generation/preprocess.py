b009f1dd2f74107fe17398b24a74181be4f868ac
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-14 10:30:35 +1100

    Add debug print statement for velocity_x_slice shape in preprocess_dedalus_to_shards function to assist with data validation.

--- data_generation/preprocess.py
+++ data_generation/preprocess.py
@@ -471,6 +471,7 @@ def preprocess_dedalus_to_shards(dataset_name='ns2d_dedalus', save_dir='./data/l
                 vorticity_slice = np.array(f['tasks/vorticity'][t], dtype=np.float32)
                 stream_slice = np.array(f['tasks/streamfunction'][t], dtype=np.float32)
                 velocity_x_slice = np.array(f['tasks/velocity'][t, 0], dtype=np.float32)
+                print("velocity_x_slice.shape", velocity_x_slice.shape)
                 velocity_y_slice = np.array(f['tasks/velocity'][t, 1], dtype=np.float32)
                 pressure_slice = np.array(f['tasks/pressure'][t], dtype=np.float32)
                 vorticity_slices.append(vorticity_slice)
