e6cf67d186103e4895138bd3bfefefdaf9b24aaa
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-13 15:51:03 +1100

    debug

--- submit_bash.sh
+++ submit_bash.sh
@@ -63,8 +63,8 @@ source $HOME/.venvs/pytorch/bin/activate
 
 ###############################################################NSE TORCHCFD ############################################################
 # Training
-# CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=3000
-python3 benchmark_dataloader.py
+CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --lr_method='cossin' --T_in=7 --epochs=3000
+# python3 benchmark_dataloader.py
 
 #################################################################################################################################
 
