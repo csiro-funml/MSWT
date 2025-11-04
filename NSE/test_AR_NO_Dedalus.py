a3c00fa98c4f903776d12b67a93123630b2fe96d
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-05 02:14:24 +1100

    Update model selection in submit_bash.sh and adjust data loading in test_AR_NO_Dedalus.py for consistent testing with the HFS model.

--- NSE/test_AR_NO_Dedalus.py
+++ NSE/test_AR_NO_Dedalus.py
@@ -483,11 +483,11 @@ def plot_spectral_error(save_data, model_name='', log_path=''):
 if __name__ == '__main__':
     
     #### 1. predict and save the data
-    model, test_loader, log_path = load_data_model(just_load_path=True)
-    # save_data = predict_and_save(model, test_loader, save=True, log_path=log_path)
+    model, test_loader, log_path = load_data_model(just_load_path=False)
+    save_data = predict_and_save(model, test_loader, save=True, log_path=log_path)
     
     # #### 2. load the save_data
-    save_data = torch.load(f'{log_path}/test_data_prediction.pth', map_location=device)
+    # save_data = torch.load(f'{log_path}/test_data_prediction.pth', map_location=device)
     
     # #### 3. compute different types of metrics
     compute_evalutation_metrics(save_data, model_name=args.model, log_path=log_path)
