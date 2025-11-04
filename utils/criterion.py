992a142becdbe03d7a10c42d98def29d3cb4d77c
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-05 00:59:33 +1100

    Refactor Energy_Enstropy_SpectrumError class in criterion.py to detach and convert prediction and target tensors to NumPy arrays for improved compatibility with subsequent computations.

--- utils/criterion.py
+++ utils/criterion.py
@@ -213,8 +213,8 @@ class Energy_Enstropy_SpectrumError(_WeightedLoss):
         y_dict = {'pred': pred, 'y': y}
         # Get fields at this time
         for y_key, y_value in y_dict.items(): # pred and y
-            psi_grid = y_value[..., 0, 1]  # streamfunction
-            omega_grid = y_value[..., 0, 0] # vorticity
+            psi_grid = y_value[0,..., 0, 1].detach().cpu().numpy()  # streamfunction
+            omega_grid = y_value[0,..., 0, 0].detach().cpu().numpy() # vorticity
 
             # Compute velocity from streamfunction
             ux_grid, uy_grid = streamfunction_to_velocity(psi_grid, Lx, Ly)
