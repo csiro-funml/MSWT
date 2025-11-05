657d12153dc831b8b39714b46fc8a0db565c5632
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-05 13:23:02 +1100

    Update submit_bash.sh to include 'use_writer' flag in the training command for enhanced logging and clarity in model training configurations.

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:30:00
+#SBATCH --time=23:30:00
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -65,7 +65,7 @@ source $HOME/.venvs/pytorch/bin/activate
 # Training
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000 --normalize_strategy='minmax'
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000 --normalize_strategy='zscore'
-CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --modes=16 --width=32 --n_layers=4 --lr=3e-4 --lr_method='cossin' --T_in=7 --epochs=5000 --normalize_strategy='zscore'
+CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --modes=16 --width=32 --n_layers=4 --lr=3e-4 --lr_method='cossin' --T_in=7 --epochs=5000 --normalize_strategy='zscore'
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='HFS' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000
 
 # Testing
