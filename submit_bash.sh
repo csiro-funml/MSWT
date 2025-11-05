1d0d33c5efdd1a4b59bc2e327f0847c517e013f6
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-05 14:11:02 +1100

    Update submit_bash.sh to switch normalization strategy in training command from 'minmax' to 'zscore' for the 'FNO' model, while commenting out the previous command for clarity in model training configurations.

--- submit_bash.sh
+++ submit_bash.sh
@@ -63,8 +63,8 @@ source $HOME/.venvs/pytorch/bin/activate
 
 ###############################################################NSE TORCHCFD ############################################################
 # Training
-CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000 --normalize_strategy='minmax'
-# CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000 --normalize_strategy='zscore'
+# CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000 --normalize_strategy='minmax'
+CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000 --normalize_strategy='zscore'
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='FNO' --use_writer --modes=16 --width=32 --n_layers=4 --lr=3e-4 --lr_method='cossin' --T_in=7 --epochs=5000 --normalize_strategy='zscore'
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/train_AR_NO.py --dataset='ns2d_dedalus' --model='HFS' --use_writer --lr_method='cossin' --T_in=7 --epochs=3000
 
