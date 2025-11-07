4fe3ec895e8285fb3658df559fd28dad0779a3b9
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-07 15:18:29 +1100

    Reorder dataset states in preprocess.py to prioritize validation and testing before training, enhancing the clarity of the preprocessing workflow.

--- data_generation/preprocess.py
+++ data_generation/preprocess.py
@@ -1070,6 +1070,6 @@ if __name__ == '__main__':
 
 
     #### Dedalus datasets
-    states = ['train', 'val', 'test']
+    states = ['val', 'test','train']
     for state in states:
         preprocess_dedalus_to_shards(dataset_name='ns2d_dedalus', save_dir='/scratch3/wan410/operator_learning_data/Dedalus/Forcing/'+state, shard_size=2048, dtype='float16', state=state)
