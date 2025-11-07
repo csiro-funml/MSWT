6bfc1ab15a5d651aee8e0d828d79505c3947f43c
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-07 16:10:22 +1100

    Enhance compute_time_avg_std.py to compute and print mean and standard deviation for each channel in 4D datasets, while maintaining functionality for 3D datasets. This improves statistical analysis of HDF5 snapshot data.

--- data_generation/dedalus/scripts/compute_time_avg_std.py
+++ data_generation/dedalus/scripts/compute_time_avg_std.py
@@ -1,13 +1,23 @@
 import os
 import h5py
+import numpy as np
+
 folder = '/datasets/work/oa-tcch/work/forXuesong/with-forcing/realisation_0000/snapshots'
 data_file = os.path.join(folder, 'snapshots_s1.h5')
 
 with h5py.File(data_file, 'r') as f:
     for key in f['tasks'].keys():
-        var = f['tasks'][key]
-        print(var)
-        print(var.shape)
-        print(var.mean())
-        print(var.std())
+        print(key)
+        var = np.array(f['tasks'][key][:])
+        # print(f"shape: {var.shape}")
+        if len(var.shape) == 4: # (T, C, H, W)
+            for c in range(var.shape[1]):
+                var_c = var[:, c, :, :]
+                print(f"shape: {var_c.shape} for channel {c}")
+                print(f"mean: {var_c.mean()}")
+                print(f"std: {var_c.std()}")
+        else: # (T, H, W)
+            print(f"shape: {var.shape}")
+            print(f"mean: {var.mean()}")
+            print(f"std: {var.std()}")
     
