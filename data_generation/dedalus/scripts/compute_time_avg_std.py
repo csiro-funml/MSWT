6a60f54093197317164c6cfa98bd4ce97f180ed7
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-07 16:05:24 +1100

    Refactor HDF5 data reading in compute_time_avg_std.py to use context manager for improved resource management. This change enhances code clarity and ensures proper file handling during data processing.

--- data_generation/dedalus/scripts/compute_time_avg_std.py
+++ data_generation/dedalus/scripts/compute_time_avg_std.py
@@ -3,11 +3,11 @@ import h5py
 folder = '/datasets/work/oa-tcch/work/forXuesong/with-forcing/realisation_0000/snapshots'
 data_file = os.path.join(folder, 'snapshots_s1.h5')
 
-data = h5py.File(data_file, 'r')
-for key in data.keys():
-    print(key)
-    var = data[key][:]
-    print(var.shape)
-    print(var.mean())
-    print(var.std())
+with h5py.File(data_file, 'r') as f:
+    for key in f.keys():
+        var = f[key]
+        print(var)
+        print(var.shape)
+        print(var.mean())
+        print(var.std())
     
