26a99cefdb115247538c33322e1e05313f1c34ce
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-07 16:07:25 +1100

    Update HDF5 data access in compute_time_avg_std.py to read from 'tasks' key instead of top-level keys, ensuring correct data retrieval for statistical analysis.

--- data_generation/dedalus/scripts/compute_time_avg_std.py
+++ data_generation/dedalus/scripts/compute_time_avg_std.py
@@ -4,8 +4,8 @@ folder = '/datasets/work/oa-tcch/work/forXuesong/with-forcing/realisation_0000/s
 data_file = os.path.join(folder, 'snapshots_s1.h5')
 
 with h5py.File(data_file, 'r') as f:
-    for key in f.keys():
-        var = f[key]
+    for key in f['tasks'].keys():
+        var = f['tasks'][key]
         print(var)
         print(var.shape)
         print(var.mean())
