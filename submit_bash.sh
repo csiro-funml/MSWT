ea810ceb32bbed4f82c463f75f9d4fa5397ed826
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-10 15:39:08 +1100

    Update submit_bash.sh to simplify training command by removing batch size parameter
    This commit modifies the SLURM submission script by removing the '--num_workers' and '--pin_memory' parameters from the training command, streamlining the configuration for clarity. The changes aim to enhance readability and maintainability of the script while keeping the focus on the 'velocity' form for the training process.

--- submit_bash.sh
+++ submit_bash.sh
@@ -102,7 +102,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # Testing
 CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
     --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore \
-    --form velocity --num_workers 8 --pin_memory --prefetch_factor 1  # Disabled for now
+    --form velocity  # Disabled for now
 
 
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_dedalus' --model='FNO' --normalize_strategy='zscore'
