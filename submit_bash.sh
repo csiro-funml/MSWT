4cda3cdd390d1f26e83845891f9c57e0e605513f
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-12 10:17:08 +1100

    Update submit_bash.sh to enable training and testing commands for the FNO model, including a resume option. Comment out the previous test command for clarity.

--- submit_bash.sh
+++ submit_bash.sh
@@ -94,16 +94,16 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 #     --form velocity --batch_size 64 --gradient_accumulation_steps 4 --epochs 1000 --num_workers 8 --pin_memory --prefetch_factor 1 --use_writer # Disabled for now
 
 
-# CUDA_VISIBLE_DEVICES=0 python3 -u NSE/train_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
-#     --modes 128 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore \
-#     --form velocity --batch_size 128 --gradient_accumulation_steps 4 --epochs 1000 --num_workers 8 --pin_memory --prefetch_factor 1 --use_writer # Disabled for now
+CUDA_VISIBLE_DEVICES=0 python3 -u NSE/train_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
+    --modes 128 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore \
+    --form velocity --batch_size 128 --gradient_accumulation_steps 4 --epochs 1000 --num_workers 8 --pin_memory --prefetch_factor 1 --resume_path=True # Disabled for now
 
 
 
 # Testing
-CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
-    --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore --epochs 1000 \
-    --form velocity  # Disabled for now
+# CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
+#     --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore --epochs 1000 \
+#     --form velocity  # Disabled for now
 
 
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_dedalus' --model='FNO' --normalize_strategy='zscore'
