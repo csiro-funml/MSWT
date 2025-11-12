Fix nyquist frequency calculation in EnergySpectrumBias class by converting to int for accurate division and added debug print statement for clarity.

utils/criterion.py
@@ -441,7 +441,8 @@ class EnergySpectrumBias(_WeightedLoss):
         k_bins, Ek_pred = compute_spectra_torch(pred[...,0,ux_dim], pred[...,0,uy_dim], Lx, Ly)
         k_bins, Ek_target = compute_spectra_torch(target[...,0,ux_dim], target[...,0,uy_dim], Lx, Ly)
         # get the nyquist frequency
-        nyquist_freq = (np.pi * pred.shape[1]) //Lx 
+        nyquist_freq = int((np.pi * pred.shape[1]) /Lx )
+        print("nyquist_freq", nyquist_freq)
         start_freq = 1
         # get the energy spectrum of the error
         if self.log_scale: