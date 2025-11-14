5d8d6235a62e3837835e2b844cb773e581037a6c
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-14 14:22:35 +1100

    Update submit_bash.sh to comment out the FNO model testing command and add a new command for analyzing predictions. This change enhances the script's functionality by allowing users to run the analysis on prediction data while keeping the testing command available for future use.

--- submit_bash.sh
+++ submit_bash.sh
@@ -101,9 +101,13 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 # # Testing
-CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
-    --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore --epochs 1000 \
-    --form velocity --num_steps 30 --dataset_type long # Disabled for now
+# CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
+#     --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore --epochs 1000 \
+#     --form velocity --num_steps 30 --dataset_type long # Disabled for now
+
+
+ python3 NSE/analyze_predictions_collaborator.py --mode explain \
+  --input_file /scratch3/wan410/operator_learning_model/ns2d_dedalus_big_FNO_mod32_wid32_lay4_ntrain32006_formvelocity_lossfourier_logscaleTrue_warmup0/test_data_prediction_long.npz
 
 # CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
 #     --modes 64 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore --epochs 1000 \
