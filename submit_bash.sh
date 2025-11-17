c10c43cf1080c7fd682ec09a42021e1258f3738f
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-17 13:16:33 +1100

    Update submit_bash.sh to correct the loss_type parameter for the FNO model testing command, ensuring it aligns with the intended configuration. This change enhances the clarity and consistency of the testing setup.

--- submit_bash.sh
+++ submit_bash.sh
@@ -160,16 +160,16 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 #      --warmup_epochs 0 --loss_type fourier --fourier_logscale False --num_steps 50000 --dataset_type long --save_type pth
 
 
-CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
-    --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore \
-    --form velocity --batch_size 64 --epochs 1000 --num_workers 8 --pin_memory --prefetch_factor 1 \
-     --warmup_epochs 300 --loss_type fourier --fourier_logscale True --num_steps 50000 --dataset_type long --save_type pth
-
-
 # CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
 #     --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore \
 #     --form velocity --batch_size 64 --epochs 1000 --num_workers 8 --pin_memory --prefetch_factor 1 \
-#      --warmup_epochs 300 --loss_type fourier --fourier_logscale False --num_steps 50000 --dataset_type long --save_type pth
+#      --warmup_epochs 300 --loss_type fourier --fourier_logscale True --num_steps 50000 --dataset_type long --save_type pth
+
+
+CUDA_VISIBLE_DEVICES=0 python3 -u NSE/test_AR_NO_Dedalus.py --dataset ns2d_dedalus_big --model FNO \
+    --modes 32 --width 32 --n_layers 4 --T_in 1 --T_ar 1  --normalize 1 --normalize_strategy zscore \
+    --form velocity --batch_size 64 --epochs 1000 --num_workers 8 --pin_memory --prefetch_factor 1 \
+     --warmup_epochs 300 --loss_type fourier --fourier_logscale False --num_steps 50000 --dataset_type long --save_type pth
 
 # CUDA_VISIBLE_DEVICES=0 python3 NSE/utils_plot.py --dataset='ns2d_dedalus' --model='FNO' --normalize_strategy='zscore'
 
