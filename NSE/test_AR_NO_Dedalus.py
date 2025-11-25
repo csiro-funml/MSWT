ef3c500258228b07e7eb527b9bc195030466e145
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-25 12:19:56 +1100

    Update model parameters in test_AR_NO_Dedalus.py to enhance performance by increasing n_layers to 8, adjusting modes to 16, and widening the width to 64. This change aims to improve model capacity and evaluation metrics.

--- NSE/test_AR_NO_Dedalus.py
+++ NSE/test_AR_NO_Dedalus.py
@@ -63,10 +63,10 @@ parser.add_argument('--normalize_strategy',type=str, default='zscore')
 parser.add_argument('--num_steps', type=int, default=300)
 
 # ### FNO/UNO params 
-parser.add_argument('--n_layers',type=int, default=4)
-# parser.add_argument('--modes', type=int, default=16)
-parser.add_argument('--modes', type=int, default=32)
-parser.add_argument('--width', type=int, default=32)
+parser.add_argument('--n_layers',type=int, default=8)
+parser.add_argument('--modes', type=int, default=16)
+# parser.add_argument('--modes', type=int, default=32)
+parser.add_argument('--width', type=int, default=64)
 # parser.add_argument('--use_ln',type=int, default=0)
 parser.add_argument('--act',type=str, default='gelu')
 
