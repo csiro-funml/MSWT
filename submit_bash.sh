10517267bf6741bccabe138279842ec4b7b80555
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-11-26 17:12:40 +1100

    Update submit_bash.sh to reduce training time from 23 hours to 6 hours, optimizing resource allocation for shorter training sessions.

--- submit_bash.sh
+++ submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=23:20:00           # Increased time for longer training with larger batches
+#SBATCH --time=6:20:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
