Refactor k_nyquist calculation in log_tensorboard_images_and_spectra function in utilities.py to restore dynamic computation from fixed value, improving accuracy in energy spectrum plotting.

utils/utilities.py
@@ -509,8 +509,7 @@ def log_tensorboard_images_and_spectra(
         
         # Create energy spectrum plot
         fig_energy, ax_energy = plt.subplots(figsize=(10, 6))
-        # k_nyquist = int((np.pi * H) // Lx)
-        k_nyquist = 63
+        k_nyquist = int((np.pi * H) // Lx)
 
         start_truth = 1
         ax_energy.loglog(k_bins[start_truth:k_nyquist], Ek_target[start_truth:k_nyquist], 