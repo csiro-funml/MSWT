50de0ab5b442b1bd63ab41356396a459764218f2
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-28 13:59:46 +1100

    Update submit_bash.sh to increase training time to 30 hours and 40 minutes for extended training with larger batches, and switch the active training configuration to SAOT model while commenting out the WNO command for clarity.

--- NS2D_ChaoticKolmogorovFlow/submit_bash.sh
+++ NS2D_ChaoticKolmogorovFlow/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=03:40:00           # Increased time for longer training with larger batches
+#SBATCH --time=30:40:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -33,7 +33,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25
-# python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25
+python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
 
 #################################################################################################################################
