e60afe723cc88a1b368d6f46fd6a80c68982c7fb
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-28 13:55:45 +1100

    Increase training time in submit_bash.sh to 2 hours and 40 minutes for extended training with larger batches.

--- NS2D_ChaoticKolmogorovFlow/submit_bash.sh
+++ NS2D_ChaoticKolmogorovFlow/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=01:20:00           # Increased time for longer training with larger batches
+#SBATCH --time=02:40:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
