bc9da5b457af4aa23f88c9a7621afedff7bb5659
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-12-29 16:14:34 +1100

    Add MultiscaleWaveletTransformer2DDecoderNoAttention model and update training configuration
    - Introduced the MultiscaleWaveletTransformer2DDecoderNoAttention class in wavelet_transform.py for enhanced 2D data processing without attention mechanisms.
    - Updated train_operator_AR_rell2_2d.py to include model initialization for the new decoder model.
    - Added a new configuration file MSWT_NodecoderAttn.yaml for training parameters specific to the Multiscale Wavelet Transformer without attention.

--- NS2D_ChaoticKolmogorovFlow/submit_bash.sh
+++ NS2D_ChaoticKolmogorovFlow/submit_bash.sh
@@ -36,6 +36,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25
 
+# python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25
+python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT_NodecoderAttn.yaml --test_ratio 0.25
 #################################################################################################################################
