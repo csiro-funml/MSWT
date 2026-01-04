42cb7af16ff07aee2cd12e58af04ac280a775789
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-04 16:33:15 +1100

    Refactor MultiscaleWaveletTransformer2D model and remove unused parameters
    - Updated train_operator_AR_rell2_2d.py to streamline model selection by removing the 'multiscale_wavelet2d_nodecoderattn_group4' variant.
    - Refactored the WaveletAttentionBlock class to eliminate the 'groups' parameter, simplifying the constructor.
    - Adjusted convolution layers in the MultiscaleWaveletTransformer2D class to remove unnecessary 'groups' argument, enhancing clarity and performance.
    - Commented out the instantiation of the MultiscaleWaveletTransformer2DDecoderNoAttention class in the main block for better code organization.

--- NS2D_ChaoticKolmogorovFlow/train_operator_AR_rell2_2d.py
+++ NS2D_ChaoticKolmogorovFlow/train_operator_AR_rell2_2d.py
@@ -362,7 +362,7 @@ def train_2d(args, config):
             use_efficient_attention=model_cfg.get('use_efficient_attention', False),
             efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
         ).to(device)
-    elif model_name in ['multiscale_wavelet2d_nodecoderattn', 'multiscale_wavelet2d_nodecoderattn_group4']:
+    elif model_name in ['multiscale_wavelet2d_nodecoderattn']:
         model = MultiscaleWaveletTransformer2DDecoderNoAttention(
             wave=model_cfg.get('wave', 'haar'),
             input_dim=model_cfg.get('in_chans', 3),
@@ -372,7 +372,6 @@ def train_2d(args, config):
             patch_size= model_cfg.get('patch_size', None),
             use_efficient_attention=model_cfg.get('use_efficient_attention', False),
             efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
-            groups=model_cfg.get('groups', 1),
         ).to(device)
     elif model_name in ['multiscale_wavelet2d_attn05124_group4']:
         model = MultiscaleWaveletTransformer2DEfficient(
--- models/wavelet_transform.py
+++ models/wavelet_transform.py
@@ -238,14 +238,14 @@ class Transformer(nn.Module):
 
 
 class WaveletAttentionBlock(nn.Module):
-    def __init__(self, wave='haar', dim=64, use_efficient_attention=False, local_attention_size=8, groups=1, **kwargs):
+    def __init__(self, wave='haar', dim=64, use_efficient_attention=False, local_attention_size=8, **kwargs):
         super().__init__(**kwargs)
         self.dwt = DWT_2D(wave)
         self.mlp_head = nn.Sequential(
             nn.Linear(dim, dim//4),
              nn.LayerNorm(dim//4))
         self.conv_post =  self.filter = nn.Sequential(
-                nn.Conv2d(dim, dim, kernel_size=3, padding=1, stride=1, groups=groups),
+                nn.Conv2d(dim, dim, kernel_size=3, padding=1, stride=1),
                 # nn.BatchNorm2d(dim),
             )
         self.idwt = IDWT_2D(wave)
@@ -328,7 +328,7 @@ class MultiscaleWaveletTransformer2D(nn.Module):
                 nn.Linear(dim, dim//4),
                 nn.LayerNorm(dim//4),
                 DWT_2D(wave), 
-                nn.Conv2d(dim, dims[i+1] if i < self.n_layers - 1 else dim, kernel_size=3, padding=1, stride=1, groups=1),
+                nn.Conv2d(dim, dims[i+1] if i < self.n_layers - 1 else dim, kernel_size=3, padding=1, stride=1),
             ])
                 
             self.enc_layers.append(nn.ModuleList([attn_layer, down_layer]))
@@ -342,7 +342,7 @@ class MultiscaleWaveletTransformer2D(nn.Module):
                 nn.Linear(dim, dim*4),
                 nn.LayerNorm(dim*4),
                 IDWT_2D(wave),
-                nn.Conv2d(2*dim, new_dim, kernel_size=3, padding=1, stride=1, groups=1),
+                nn.Conv2d(2*dim, new_dim, kernel_size=3, padding=1, stride=1),
                 ])
                 
             attn_layer = nn.ModuleList([
@@ -425,135 +425,15 @@ class MultiscaleWaveletTransformer2D(nn.Module):
 
 
 
-class MultiscaleWaveletTransformer2DDecoderNoAttention(nn.Module):
-    def __init__(self, wave='haar', input_dim=3, output_dim=3, dim=None, dims=[], use_efficient_attention=False,
-                       efficient_layers=[0, 1], add_grid=False, patch_size=None, groups=1, **kwargs):
-        super().__init__(**kwargs)
-        self.add_grid = add_grid
-        self.patch_size = patch_size
 
-        if len(dims) == 0 and dim is not None:
-            dims = np.array([dim//4, dim, 4*dim, 4*dim])
-        else:
-            dims = np.array(dims)
-        self.n_layers = len(dims)
-        if patch_size is None:
-            self.input_proj = nn.Linear(input_dim, dims[0])
-            self.output_proj = nn.Sequential(nn.Linear(dims[0], dims[0]//2),
-                                            nn.GELU(),
-                                            nn.Linear(dims[0]//2, output_dim))
-
-        self.enc_layers = nn.ModuleList([])
-        self.use_efficient_attention = use_efficient_attention
-        for i in range(self.n_layers):
-            dim = dims[i]
-            efficient_flag = i in efficient_layers and self.use_efficient_attention
-            attn_layer = nn.ModuleList([
-                nn.LayerNorm(dim),
-                WaveletAttentionBlock(wave=wave, dim=dim, use_efficient_attention=efficient_flag),
-                nn.LayerNorm(dim),
-                FeedForward(dim, dim*4)
-                ])
-
-            down_layer = nn.ModuleList([
-                nn.Linear(dim, dim//4),
-                nn.LayerNorm(dim//4),
-                DWT_2D(wave), 
-                nn.Conv2d(dim, dims[i+1] if i < self.n_layers - 1 else dim, kernel_size=3, padding=1, stride=1, groups=groups),
-            ])
-                
-            self.enc_layers.append(nn.ModuleList([attn_layer, down_layer]))
-        # self.norm = nn.LayerNorm(dim)
-        self.dec_layers = nn.ModuleList([])
-        for i in range(self.n_layers):
-            dim = dims[self.n_layers - i - 1]
-            new_dim = dims[self.n_layers - i - 2] if self.n_layers - i - 2 >= 0 else dim
-            efficient_flag = self.n_layers - i - 1 in efficient_layers and self.use_efficient_attention
-            up_layer = nn.ModuleList([
-                nn.Linear(dim, dim*4),
-                nn.LayerNorm(dim*4),
-                IDWT_2D(wave),
-                nn.Conv2d(2*dim, new_dim, kernel_size=3, padding=1, stride=1, groups=groups),
-                ])
-            
-            self.dec_layers.append(up_layer)
-
-    def get_grid(self, x):
-        b, h, w, _ = x.shape
-        size_x, size_y = h, w
-        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float).to(x.device)
-        gridx = gridx.reshape(1, size_x, 1, 1).repeat([b, 1, size_y, 1]) 
-        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float).to(x.device)
-        gridy = gridy.reshape(1, 1, size_y, 1).repeat([b, size_x, 1, 1]) 
-        x_grid = torch.cat((gridx, gridy), dim=-1)
-
-        return x_grid
-
-    def attention_block(self, x, layer, h, w):
-        ln1, wavelet_block, ln2, ff = layer
-        x = wavelet_block(ln1(x), h, w) + x
-        x = ln2(ff(x)) + x
-        return x
-    
-    def down_block(self, x, layer, h, w):
-        linear, ln, dwt, conv = layer
-        x = ln(linear(x))
-        x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
-        x = dwt(x) # (B, 4xc//4, H/2, W/2)
-        x = conv(x)
-        h, w= x.shape[-2], x.shape[-1]
-        x = rearrange(x, 'b c h w -> b (h w) c')
-        return x, h, w
-    
-
-    def up_block(self, x, x_prev, layer, h, w):
-        linear, ln, idwt, conv = layer
-        x = ln(linear(x)) # (B, (H/2 x W/2), c*4)
-        x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
-        x = idwt(x) # (B, C, H, W)
-        x = torch.cat((x, x_prev), dim=1) # (B, 2C, H, W)
-        x = conv(x)
-        h, w= x.shape[-2], x.shape[-1]
-        x = rearrange(x, 'b c h w -> b (h w) c')
-        return x, h, w
-
-    def forward(self, x):
-        if self.add_grid:
-            x_grid = self.get_grid(x)
-            x = torch.cat((x, x_grid), dim=-1)
-
-        x = self.input_proj(x)
-        h, w = x.shape[1], x.shape[2]
-        x = rearrange(x, 'b h w c -> b (h w) c')
-
-        x_list = []
-
-        for attn_layer, down_layer in self.enc_layers:
-            x_list.append(rearrange(x, 'b (h w) c -> b c h w', h=h, w=w))
-            x = self.attention_block(x, attn_layer, h, w)
-            x, h, w = self.down_block(x, down_layer, h, w) # h and w would be updated here
-            
-        
-        for up_layer in self.dec_layers:
-            x, h, w = self.up_block(x,  x_list.pop(), up_layer, h, w)
-        
-        # x = self.norm(x)
-        x = self.output_proj(x)
-        x = rearrange(x, 'b (h w) c-> b h w c', h=h, w=w)
-        return x
-
-
-    def count_parameters(self):
-        return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
 
 
 if __name__ == "__main__":
     x = torch.randn(2, 64, 64, 3)
-    # x = torch.rand(2, 96, 192, 3)
-    # model = MultiscaleWaveletTransformer2D(input_dim=3, output_dim=3, dim=64, use_efficient_attention=True)
+    model = MultiscaleWaveletTransformer2D(input_dim=3, output_dim=1, dim=64, use_efficient_attention=True)
     
-    model = MultiscaleWaveletTransformer2DDecoderNoAttention(input_dim=3, output_dim=1, dim=128, use_efficient_attention=True, groups=4)
+    # model = MultiscaleWaveletTransformer2DDecoderNoAttention(input_dim=3, output_dim=1, dim=96, use_efficient_attention=True)
     
     print("number of parameters:", model.count_parameters())
     with torch.autograd.set_detect_anomaly(True):
