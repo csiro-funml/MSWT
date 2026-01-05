a5349e551ce9415b1b10d28b39ab1c19c797479d
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-05 20:48:43 +1100

    Add testing script and update configuration for FNO model
    - Introduced a new testing script, test_operator_AR_rell2_2d.py, for evaluating the FNO model's performance.
    - Updated submit_bash.sh to enable the FNO testing command and commented out the previous SAOT testing command.
    - Modified FNO.yaml configuration to specify new test data paths and parameters for improved evaluation.

--- SW2D_PDA/submit_bash.sh
+++ SW2D_PDA/submit_bash.sh
@@ -33,7 +33,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefinerUNet.yaml --test_ratio 0.25
@@ -45,7 +45,7 @@ python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_rat
 
 
 # Testing
-# python3 test_operator_AR_rell2_2d.py --config_path configs/FNO.yaml
+python3 test_operator_AR_rell2_2d.py --config_path configs/FNO.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/HFS.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/WNO.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml
