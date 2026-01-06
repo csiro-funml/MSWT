b3b112a4eb06bc2eb69721ffb3ba5c39a54237ad
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-07 05:46:17 +1100

    Update submit_bash.sh to comment out the SAOT model execution and enable MSWT model execution for training.

--- SW2D_PDA/submit_bash.sh
+++ SW2D_PDA/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=10:10:00           # Increased time for longer training with larger batches
+#SBATCH --time=05:10:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
