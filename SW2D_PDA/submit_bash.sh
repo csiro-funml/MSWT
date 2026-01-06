ad698f810cf967aa1ee2bd2e14f4eb873cd189e8
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-06 16:34:22 +1100

    Refactor train_operator_AR_rell2_2d.py for improved input handling and update submit_bash.sh for training script execution
    - Modified input concatenation in evaluate_step_ahead and train_step_ahead functions to remove unnecessary dimensions.
    - Adjusted data indexing in get_fixed_test_pair to correctly access the last time step.
    - Updated submit_bash.sh to change SLURM job time to 3:10:00 and uncomment the execution command for PDERefinerUNet configuration.

--- SW2D_PDA/submit_bash.sh
+++ SW2D_PDA/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=10:10:00           # Increased time for longer training with larger batches
+#SBATCH --time=3:10:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -32,11 +32,11 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25 --resume_training
-python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25 --resume_training
+# python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25 
 # python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25 --resume_training
-# python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefinerUNet.yaml --test_ratio 0.25 --resume_training
+python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefinerUNet.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_double_attn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn_Group4.yaml --test_ratio 0.25
