a868752cff2e6dabe0d0c0d8cef5c131e52d26e9
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-06 18:21:42 +1100

    Increase job time limit in submit_bash.sh to 15 hours and enable MSWT model execution while commenting out the HFS model execution line.

--- NS2D_Dedalus/submit_bash.sh
+++ NS2D_Dedalus/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=10:00:00           # Increased time for longer training with larger batches
+#SBATCH --time=15:00:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -35,11 +35,11 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 # python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25
-# python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25
+python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefinerUNet.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_double_attn.yaml --test_ratio 0.25
