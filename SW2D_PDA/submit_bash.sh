ac51eec6f9b1bd6ea86b62afc43daa757cd30e89
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-09 17:03:17 +1100

    Update submit_bash.sh to enable the HFS configuration for training and comment out the MSWT_periodic configuration, streamlining the execution commands for clarity.

--- SW2D_PDA/submit_bash.sh
+++ SW2D_PDA/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=20:40:00           # Increased time for longer training with larger batches
+#SBATCH --time=00:40:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -31,7 +31,7 @@ echo "=========================================="
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25 --resume_training
-# python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
+python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25 
@@ -41,7 +41,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_double_attn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn_Group4.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_DeNoAttn_StackLayers.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic.yaml --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic.yaml --test_ratio 0.25
 #################################################################################################################################
 
 
