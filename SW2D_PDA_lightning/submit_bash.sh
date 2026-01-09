aa21415b611ae792bc29e9c1cf65dcb0be68ae8d
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-09 16:38:50 +1100

    Update SLURM job configuration in submit_bash.sh: increased time limit to 20 hours for extended training, and enabled the MSWT_periodic configuration for execution. Added a new submit_bash.sh file for the SW2D_PDA_lightning framework with enhanced resource allocation settings.

--- SW2D_PDA_lightning/submit_bash.sh
+++ SW2D_PDA_lightning/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=20:40:00           # Increased time for longer training with larger batches
+#SBATCH --time=00:40:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -35,13 +35,13 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefiner.yaml --test_ratio 0.25 
-# python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25 
+python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefinerUNet.yaml --test_ratio 0.25 --resume_training
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_double_attn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn_Group4.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_DeNoAttn_StackLayers.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic.yaml --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic.yaml --test_ratio 0.25
 #################################################################################################################################
 
 
