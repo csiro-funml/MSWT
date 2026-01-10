237b68f3ebaaf9e8fc4b24a850dadfe04bc2d3ce
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-11 09:24:16 +1100

    Update submit_bash.sh to reduce training time limit to 00:10:00 and adjust the order of test commands for periodic models.

--- SW2D_PDA/submit_bash.sh
+++ SW2D_PDA/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:30:00           # Increased time for longer training with larger batches
+#SBATCH --time=00:10:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -58,7 +58,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_NodecoderAttn.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_double_attn.yaml
-python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic.yaml
-# python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic_nlayers4.yaml
+# python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic.yaml
+python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic_nlayers4.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/MSWT_periodic_patching.yaml
 #################################################################################################################################
