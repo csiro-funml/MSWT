03db4cd87238821108614aa7e3a525d4e821fe0e
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-12 10:57:45 +1100

    Update submit_bash.sh to adjust training time limit and resume training option
    - Changed the training time limit from 01:10:00 to 00:30:00 for efficiency.
    - Added the `--resume_training` flag to the command for the `MSWT_periodic_patching` model to allow continuation of training from a previous checkpoint.

--- NS2D_ChaoticKolmogorovFlow/submit_bash.sh
+++ NS2D_ChaoticKolmogorovFlow/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=01:10:00           # Increased time for longer training with larger batches
+#SBATCH --time=00:30:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -42,7 +42,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_double_attn.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_NodecoderAttn_Group4.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_DeNoAttn_StackLayers.yaml --test_ratio 0.25
-python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_periodic_patching.yaml --test_ratio 0.25
+python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/MSWT_periodic_patching.yaml --test_ratio 0.25 --resume_training
 #################################################################################################################################
 
 
