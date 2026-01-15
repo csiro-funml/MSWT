3924e3e75b4a9a60a4e3c6822dfbfd39798c2526
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-15 23:52:46 +1100

    Refactor configuration management by removing deprecated `PDERefiner_periodic.yaml` and introducing new model-specific configuration files: `FNO_periodic.yaml`, `HFS_periodic.yaml`, `MSWT_patching_periodic.yaml`, `PDERefinerUNet_periodic.yaml`, `SAOT_periodic.yaml`, and `WNO_periodic.yaml`, to enhance training options for the NS2D Chaotic Kolmogorov Flow project.

--- NS2D_ChaoticKolmogorovFlow/train_operator_AR_rell2_2d.py
+++ NS2D_ChaoticKolmogorovFlow/train_operator_AR_rell2_2d.py
@@ -15,7 +15,6 @@ from models.high_frequency_scaling import ResUNet
 from models.wno import WNO2d
 from models.saot import SAOTModel
 from models.wavelet_transform import MultiscaleWaveletTransformer2D
-from models.wavelet_transform_exploration import MultiscaleWaveletTransformer2DDecoderNoAttention, MultiscaleWaveletTransformer2DEfficient, MultiscaleWaveletDoubleAttention, MSWT_DeNoAttn_StackLayers
 from models.periodic_mswt import PeriodicMSWT2D_Patching
 from models.pderefiner import PDERefiner
 from models.pderefiner_unet import UNetRefiner
@@ -363,46 +362,6 @@ def train_2d(args, config):
             dim=model_cfg.get('dim', None),
             dims=model_cfg.get('dims', []),
             patch_size= model_cfg.get('patch_size', None),
-            use_efficient_attention=model_cfg.get('use_efficient_attention', False),
-            efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
-        ).to(device)
-    elif model_name in ['multiscale_wavelet2d_nodecoderattn']:
-        model = MultiscaleWaveletTransformer2DDecoderNoAttention(
-            wave=model_cfg.get('wave', 'haar'),
-            input_dim=model_cfg.get('in_chans', 3),
-            output_dim=model_cfg.get('out_chans', 1),
-            dim=model_cfg.get('dim', None),
-            dims=model_cfg.get('dims', []),
-            patch_size= model_cfg.get('patch_size', None),
-            use_efficient_attention=model_cfg.get('use_efficient_attention', False),
-            efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
-        ).to(device)
-    elif model_name in ['multiscale_wavelet2d_attn05124_group4']:
-        model = MultiscaleWaveletTransformer2DEfficient(
-            wave=model_cfg.get('wave', 'haar'),
-            input_dim=model_cfg.get('in_chans', 3),
-            output_dim=model_cfg.get('out_chans', 1),
-            dim=model_cfg.get('dim', None),
-            dims=model_cfg.get('dims', []),
-            patch_size= model_cfg.get('patch_size', None),
-            use_efficient_attention=model_cfg.get('use_efficient_attention', False),
-            efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
-        ).to(device)
-    elif model_name in ['multiscale_wavelet2d_double_attn']:
-        model = MultiscaleWaveletDoubleAttention(
-            wave=model_cfg.get('wave', 'haar'),
-            input_dim=model_cfg.get('in_chans', 3),
-            output_dim=model_cfg.get('out_chans', 1),
-            dim=model_cfg.get('dim', None),
-            use_efficient_attention=model_cfg.get('use_efficient_attention', False),
-            efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
-        ).to(device)
-    elif model_name in ['multiscale_wavelet2d_denoattn_stacklayers3']:
-        model = MSWT_DeNoAttn_StackLayers(
-            wave=model_cfg.get('wave', 'haar'),
-            input_dim=model_cfg.get('in_chans', 3),
-            output_dim=model_cfg.get('out_chans', 1),
-            dims=model_cfg.get('dims', []),
             use_efficient_attention=model_cfg.get('use_efficient_attention', False),
             efficient_layers=model_cfg.get('efficient_layers', [0, 1, 2]),
         ).to(device)
