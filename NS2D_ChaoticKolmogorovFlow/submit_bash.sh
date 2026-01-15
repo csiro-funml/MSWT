9999c13442beb40d879eade9048f9eec4e2202f2
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-15 12:29:32 +1100

    Update `submit_bash.sh` to change the configuration path for testing from `FNO.yaml` to `FNO_periodic.yaml`, ensuring the correct settings are used for model evaluation.

--- NS2D_ChaoticKolmogorovFlow/submit_bash.sh
+++ NS2D_ChaoticKolmogorovFlow/submit_bash.sh
@@ -52,7 +52,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 # Testing
-python3 test_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_seed 42
+python3 test_operator_AR_rell2_2d.py --config_path configs/FNO_periodic.yaml --test_seed 42
 # python3 test_operator_AR_rell2_2d.py --config_path configs/HFS.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/WNO.yaml
 # python3 test_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml
