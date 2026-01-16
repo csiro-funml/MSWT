4970bee85b7b2f3d380f06dad02127e6f51a5fb3
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-17 07:46:57 +1100

    Refactor CSV file processing in `post_processing_metric_table.py` to streamline metric aggregation by removing nested loops for seed iteration. This change enhances code readability and maintains functionality for combining evaluation metrics across models.

--- NS2D_ChaoticKolmogorovFlow/post_processing_metric_table.py
+++ NS2D_ChaoticKolmogorovFlow/post_processing_metric_table.py
@@ -9,15 +9,14 @@ if __name__ == "__main__":
     seeds = [42, 43, 44, 45, 46]
     total_df_metric = pd.DataFrame()
     for model_name in model_name_list:
-        for seed in seeds:
-            path_folder = os.path.join(save_folder, f'{model_name}2d_linear/evaluation_metrics')
-            file_list = os.listdir(path_folder)
-            for file in file_list:
-                if file.endswith('.csv'):
-                    df_metric = pd.read_csv(os.path.join(path_folder, file))
-                    # print(df_metric.head())
-                    # df_metric = df.iloc[0]
-                    total_df_metric = pd.concat([total_df_metric, df_metric], ignore_index=True)
+        path_folder = os.path.join(save_folder, f'{model_name}2d_linear/evaluation_metrics')
+        file_list = os.listdir(path_folder)
+        for file in file_list:
+            if file.endswith('.csv'):
+                df_metric = pd.read_csv(os.path.join(path_folder, file))
+                # print(df_metric.head())
+                # df_metric = df.iloc[0]
+                total_df_metric = pd.concat([total_df_metric, df_metric], ignore_index=True)
 
     total_df_metric.to_csv(os.path.join(save_folder, 'total_evaluation_metrics.csv'), index=False)
 
