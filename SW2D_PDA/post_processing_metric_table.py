1284b148f78abfc5e2fb1a4078e4628fafad05b1
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-01-20 06:35:03 +1100

    Update error plotting steps in post_processing_metric_table.py to include new indices for improved visualization. This change enhances the granularity of the generated error plots, aligning with recent adjustments in the plotting function.

--- SW2D_PDA/post_processing_metric_table.py
+++ SW2D_PDA/post_processing_metric_table.py
@@ -314,7 +314,7 @@ def plot_error():
         save_folder = "logs/SW2D_PDA/"
     # steps = [1, 41, 87]
     # steps = range(0, 88, 20)
-    steps = [1, 41, 81]
+    steps = [0, 40, 80]
     # steps = [80]
     dataset_name = 'SW2D_PDA'
     # model_name_list = ['FNO', 'PDERefinerUNet', 'SAOT', 'HFS', 'MSWT_patching']
