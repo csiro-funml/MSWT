7e7e651012f0fd69c1adf93511d756b02b8a07f8
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-10 17:01:54 +1100

    Update submit_bash_test.sh to enable testing on NS2D and SW2D datasets
    - Uncommented and activated testing commands for the Multiscale Wavelet Transformer (MSWT) on both NS2D and SW2D datasets, ensuring the script is ready for execution.
    - Maintained consistency in configuration paths for the respective datasets, facilitating streamlined testing processes.

--- submit_bash_test.sh
+++ submit_bash_test.sh
@@ -16,10 +16,10 @@ module load pytorch/2.5.1-py312-cu122-mpi
 source /datasets/work/oa-turb-arc/work/multiscale_wavelet_transformers_Xuesong_ICML2026/venv/bin/activate
 
 # test MSWT on NS2D
-# python3 NS2D_ChaoticKolmogorovFlow/test_operator_AR_rell2_2d.py --config_path NS2D_ChaoticKolmogorovFlow/configs/new_dataset_direc/MSWT.yaml 
+python3 NS2D_ChaoticKolmogorovFlow/test_operator_AR_rell2_2d.py --config_path NS2D_ChaoticKolmogorovFlow/configs/new_dataset_direc/MSWT.yaml 
 
 # test MSWT on SW2D
-# python3 SW2D_PDA/test_operator_AR_rell2_2d.py --config_path SW2D_PDA/configs/new_dataset_direc/MSWT_periodic.yaml 
+python3 SW2D_PDA/test_operator_AR_rell2_2d.py --config_path SW2D_PDA/configs/new_dataset_direc/MSWT_periodic.yaml 
 
 # test MSWT on ERA5
 python3 ERA5/2d_test_rel_l2.py --config_path ERA5/configs/new_dataset_direc/MSWT_sphere.yaml 
