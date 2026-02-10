cc0a8be34a7e388ec8ac6371e98d0ee148037656
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-10 16:32:28 +1100

    Add submit_bash_test.sh for testing MSWT on SW2D dataset
    - Created a new bash script to facilitate testing of the Multiscale Wavelet Transformer (MSWT) on the SW2D dataset.
    - Configured SLURM parameters for resource allocation, including memory, CPU, and GPU specifications.
    - Included commands to load the necessary PyTorch module and activate the virtual environment.
    - Added a placeholder for testing MSWT on NS2D, ensuring flexibility for future testing scenarios.

---
+++ submit_bash_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+#SBATCH --time=00:20:00           # Increased time for longer training with larger batches
+
+#SBATCH --mem=128gb
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=OD-230881
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=32        # Increased CPUs for DataLoader workers (H100 can handle more)
+#SBATCH --output=slurm-%j.out     # Explicit output file (job ID will be inserted)
+#SBATCH --error=slurm-%j.err      # Explicit error file
+
+module load pytorch/2.5.1-py312-cu122-mpi
+source /datasets/work/oa-turb-arc/work/multiscale_wavelet_transformers_Xuesong_ICML2026/venv/bin/activate
+
+# test MSWT on NS2D
+# python3 NS2D_ChaoticKolmogorovFlow/test_operator_AR_rell2_2d.py --config_path NS2D_ChaoticKolmogorovFlow/configs/new_dataset_direc/MSWT.yaml 
+
+# test MSWT on SW2D
+python3 SW2D_PDA/test_operator_AR_rell2_2d.py --config_path SW2D_PDA/configs/new_dataset_direc/MSWT.yaml 
+
+# test MSWT on ERA5
