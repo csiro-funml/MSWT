f42daa4d6438a4ae16bb3174d401e5b9139f0f47
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-11 16:55:34 +1100

    Update training command in `submit_bash.sh` to include HFS configuration
    - Uncommented the training command for `HFS.yaml` to enable testing with this configuration.
    - Maintained the command for `FNO.yaml` to ensure both configurations are available for training.

--- NS2D_Dedalus/submit_bash.sh
+++ NS2D_Dedalus/submit_bash.sh
@@ -34,8 +34,8 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 data_utils/datasets.py
 
 
-python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25
-# python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/FNO.yaml --test_ratio 0.25
+python3 train_operator_AR_rell2_2d.py --config_path configs/HFS.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/WNO.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/SAOT.yaml --test_ratio 0.25
 # python3 train_operator_AR_rell2_2d.py --config_path configs/PDERefinerUNet.yaml --test_ratio 0.25
