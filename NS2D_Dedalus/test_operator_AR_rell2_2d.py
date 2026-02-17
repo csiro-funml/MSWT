d2a139821ae13a930208f5d805243315abef1144
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-17 14:14:16 +1100

    Update input tensor handling in `test_operator_AR_rell2_2d.py` to include batch dimension
    - Added an unsqueeze operation to the input tensor `x_in` to incorporate a batch dimension, ensuring compatibility with the model's input requirements during autoregressive prediction.

--- NS2D_Dedalus/test_operator_AR_rell2_2d.py
+++ NS2D_Dedalus/test_operator_AR_rell2_2d.py
@@ -40,7 +40,7 @@ def autoregressive_predict(model, sequences, device, grid, max_time_steps=100):
             x_forcing = sequences[t][0][..., :2].to(device)
             x_prev = x_prev.to(device)
             print("x_forcing shape: ", x_forcing.shape, "x_prev shape: ", x_prev.shape, "grid shape: ", grid.shape)
-            x_in = torch.cat((x_forcing, x_prev, grid), dim=-1).to(device)
+            x_in = torch.cat((x_forcing, x_prev, grid), dim=-1).unsqueeze(0).to(device) # added the batch dimension 
             pred = model(x_in)
             x_prev = pred
             
