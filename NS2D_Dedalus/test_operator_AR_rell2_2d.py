528f6e1e89384eb5974aad3f0223af4e140aa820
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-17 14:21:49 +1100

    Refactor tensor handling in `test_operator_AR_rell2_2d.py` for autoregressive prediction
    - Removed unnecessary dimension squeezing for predictions and ground truth, simplifying tensor operations.
    - Updated ground truth extraction to ensure consistent tensor shapes during evaluation.
    - Improved overall clarity and efficiency of the prediction logic.

--- NS2D_Dedalus/test_operator_AR_rell2_2d.py
+++ NS2D_Dedalus/test_operator_AR_rell2_2d.py
@@ -39,20 +39,16 @@ def autoregressive_predict(model, sequences, device, grid, max_time_steps=100):
             # concatenate the ground truth forcing sequence[t][..., :2],  the prediction from the last step / initial condition x0,  grid  to form the input x_in
             x_forcing = sequences[t][0][..., :2].to(device)
             x_prev = x_prev.to(device)
-            print("x_forcing shape: ", x_forcing.shape, "x_prev shape: ", x_prev.shape, "grid shape: ", grid.shape)
+            # print("x_forcing shape: ", x_forcing.shape, "x_prev shape: ", x_prev.shape, "grid shape: ", grid.shape)
             x_in = torch.cat((x_forcing, x_prev, grid), dim=-1).unsqueeze(0).to(device) # added the batch dimension 
             pred = model(x_in)
             x_prev = pred.squeeze(0)
             
-            if pred.dim() == 5:
-                pred = pred.squeeze(-2)
-            if pred.dim() == 4:
-                pred = pred.squeeze(-1)
             total_pred.append(pred)
-            total_truth.append(sequences[t][1].to(device)) # the second aurgument is ground truth vorticity
+            total_truth.append(sequences[t][1].unsqueeze(0).to(device)) # the second aurgument is ground truth vorticity
         
-        total_pred = torch.stack(total_pred, dim=0)
-        total_truth = torch.stack(total_truth, dim=0)
+        total_pred = torch.cat(total_pred, dim=0).squeeze(-1) # (T, H, W, C) or (T, H, W)
+        total_truth = torch.cat(total_truth, dim=0).squeeze(-1) # (T, H, W, C) or (T, H, W)
         print("total_pred shape: ", total_pred.shape, "total_truth shape: ", total_truth.shape)
         return total_pred, total_truth
 
