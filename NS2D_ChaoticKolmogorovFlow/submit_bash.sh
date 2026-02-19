d8a88145a4d87920ff326760d042d35c7a939918
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-19 11:35:01 +1100

    Update Python version to 3.12 in `requirements.txt` and adjust job time limit in `submit_bash.sh` to 1.5 hours. Modify ablation script commands to include `--test_ratio 0.25` for improved testing configuration clarity.

--- NS2D_ChaoticKolmogorovFlow/submit_bash.sh
+++ NS2D_ChaoticKolmogorovFlow/submit_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#SBATCH --time=00:05:00           # Increased time for longer training with larger batches
+#SBATCH --time=01:30:00           # Increased time for longer training with larger batches
 
 #SBATCH --mem=256gb
 #SBATCH --nodes=1
@@ -53,11 +53,11 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 # Ablations
-# python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_attention.yaml --test_seed 42
-# python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_tokenizer.yaml --test_seed 42
-# python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_strided_up_downsampling.yaml --test_seed 42
+python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_attention.yaml --test_seed 42 --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_tokenizer.yaml --test_seed 42 --test_ratio 0.25
+# python3 train_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_strided_up_downsampling.yaml --test_seed 42 --test_ratio 0.25
 
 
-python3 test_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_attention.yaml --test_seed 42
+# python3 test_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_attention.yaml --test_seed 42
 # python3 test_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_no_tokenizer.yaml --test_seed 42
 # python3 test_operator_AR_rell2_2d.py --config_path configs/ablations/rebutall/MSWT_strided_up_downsampling.yaml --test_seed 42
