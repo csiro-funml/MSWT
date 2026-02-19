8503a6a1e45deb07807e07f95da3536c4b40cc50
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-20 10:42:14 +1100

    Add `guess_dt` function in `criterion.py` to estimate time step based on PDE equations. Integrate `dt_guess` calculation in `PINO_loss3d` for improved loss computation and debugging.

--- utils/criterion.py
+++ utils/criterion.py
@@ -60,6 +60,43 @@ def FDM_NS_vorticity(w, v=1/40, t_interval=1.0):
     return Du1
 
 
+def guess_dt(w, v, f):
+    batchsize = w.size(0)
+    nx = w.size(1)
+    ny = w.size(2)
+    nt = w.size(3)
+    device = w.device
+    w = w.reshape(batchsize, nx, ny, nt)
+
+    w_h = torch.fft.fft2(w, dim=[1, 2])
+    # Wavenumbers in y-direction
+    k_max = nx//2
+    N = nx
+    k_x = torch.cat((torch.arange(start=0, end=k_max, step=1, device=device),
+                     torch.arange(start=-k_max, end=0, step=1, device=device)), 0).reshape(N, 1).repeat(1, N).reshape(1,N,N,1)
+    k_y = torch.cat((torch.arange(start=0, end=k_max, step=1, device=device),
+                     torch.arange(start=-k_max, end=0, step=1, device=device)), 0).reshape(1, N).repeat(N, 1).reshape(1,N,N,1)
+    # Negative Laplacian in Fourier space
+    lap = (k_x ** 2 + k_y ** 2)
+    lap[0, 0, 0, 0] = 1.0
+    f_h = w_h / lap
+
+    ux_h = 1j * k_y * f_h
+    uy_h = -1j * k_x * f_h
+    wx_h = 1j * k_x * w_h
+    wy_h = 1j * k_y * w_h
+    wlap_h = -lap * w_h
+
+    ux = torch.fft.irfft2(ux_h[:, :, :k_max + 1], dim=[1, 2])
+    uy = torch.fft.irfft2(uy_h[:, :, :k_max + 1], dim=[1, 2])
+    wx = torch.fft.irfft2(wx_h[:, :, :k_max+1], dim=[1,2])
+    wy = torch.fft.irfft2(wy_h[:, :, :k_max+1], dim=[1,2])
+    wlap = torch.fft.irfft2(wlap_h[:, :, :k_max+1], dim=[1,2])
+
+    residual = f.repeat(batchsize, 1, 1, nt-2) - (ux*wx + uy*wy - v*wlap)[...,1:-1] #- forcing
+    return residual
+
+
 def PINO_loss3d(u, u0, forcing, v=1/40, t_interval=1.0):
     batchsize = u.size(0)
     nx = u.size(1)
@@ -73,6 +110,11 @@ def PINO_loss3d(u, u0, forcing, v=1/40, t_interval=1.0):
     loss_ic = lploss(u_in, u0)
     # PDE loss
     Du = FDM_NS_vorticity(u, v, t_interval)
+
+    # guess the ground truth dt based on the rest of the equations:
+    dt_guess = guess_dt(u, forcing)
+    print(f'dt_guess: {dt_guess}')
+    exit(-1)
     f = forcing.repeat(batchsize, 1, 1, nt-2)
     loss_f = lploss(Du, f)
 
