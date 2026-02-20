25a7f022dc1705b5c3d1e1aae3f68a0ddf0731a0
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-20 15:15:27 +1100

    Refactor `train_step_ahead` function in `train_operator_AR_rell2_2d.py` to ensure checkpoint saving occurs before logging fixed predictions. This change improves the training flow by breaking the loop after saving the checkpoint, allowing for better management of evaluation metrics.

--- SW2D_Dedalus/train_operator_AR_rell2_2d.py
+++ SW2D_Dedalus/train_operator_AR_rell2_2d.py
@@ -219,14 +219,16 @@ def train_step_ahead(model, train_loader, optimizer, scheduler, config, device, 
             print(f'Random test split relative L2: {test_l2:.6f}')
             if writer is not None:
                 writer.add_scalar('eval/test_l2', test_l2, ep + 1)
-                fixed_pred, fixed_target = get_fixed_test_pair(model, test_loader, grid, device, sample_idx=550, t_idx=0)
-                # print("fixed_pred shape:", fixed_pred.shape, "fixed_target shape:", fixed_target.shape)
-                
                 save_checkpoint(config['train']['save_dir'],
                                 config['train']['save_name'],
                                 model, 
                                 ep,
                                 optimizer, scheduler)
+                break
+                fixed_pred, fixed_target = get_fixed_test_pair(model, test_loader, grid, device, sample_idx=550, t_idx=0)
+                # print("fixed_pred shape:", fixed_pred.shape, "fixed_target shape:", fixed_target.shape)
+                
+                
                 if fixed_pred is not None:
                     log_tensorboard_images_and_spectra(writer,
                                                        fixed_pred.unsqueeze(-2)[...,[0]],  # the first channel is vorticity
