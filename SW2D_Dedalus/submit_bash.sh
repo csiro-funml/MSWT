88e077479845e6a9e2cfccba95a4a8ef42587468
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-20 14:55:35 +1100

    Update `submit_bash.sh` to include a new training command for `SFNO.yaml` and comment out the corresponding test command, enhancing the script's functionality for specific model testing.

--- SW2D_Dedalus/submit_bash.sh
+++ SW2D_Dedalus/submit_bash.sh
@@ -34,7 +34,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 train_operator_AR_rell2_2d.py --config_path configs/spherical/FNO_spherical.yaml
 # python3 train_operator_AR_rell2_2d.py --config_path configs/spherical/HFS_spherical.yaml
 # python3 train_operator_AR_rell2_2d.py --config_path configs/spherical/MSWT_patching_spherical.yaml
-
+python3 train_operator_AR_rell2_2d.py --config_path configs/spherical/SFNO.yaml --test_seed 42
 #################################################################################################################################
 
 
@@ -42,5 +42,5 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # python3 test_operator_AR_rell2_2d.py --config_path configs/spherical/FNO_spherical.yaml --test_seed 42
 # python3 test_operator_AR_rell2_2d.py --config_path configs/spherical/HFS_spherical.yaml --test_seed 42
 # python3 test_operator_AR_rell2_2d.py --config_path configs/spherical/MSWT_patching_spherical.yaml --test_seed 42
-python3 test_operator_AR_rell2_2d.py --config_path configs/spherical/SFNO.yaml --test_seed 42
+# python3 test_operator_AR_rell2_2d.py --config_path configs/spherical/SFNO.yaml --test_seed 42
 #################################################################################################################################
