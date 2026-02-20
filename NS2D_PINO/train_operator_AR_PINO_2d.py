8d6b3ecea831e2d45429420fd0655ff66cf95222
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-20 11:09:03 +1100

    Add SLURM job submission script `submit_bash.sh` for training and testing PINO model. Includes resource allocation settings and job information logging.

--- NS2D_PINO/train_operator_AR_PINO_2d.py
+++ NS2D_PINO/train_operator_AR_PINO_2d.py
@@ -279,7 +279,7 @@ def train_2d(args, config):
                                     nx=data_config['nx'], nt=data_config['nt'],
                                     sub=data_config['sub'], sub_t=data_config['sub_t'],
                                     N=data_config['total_num'],
-                                    t_interval=data_config['time_interval'],
+                                    t_interval=data_config['t_duration'],
                                     n_samples=data_config.get('n_sample', data_config.get('n_samples', data_config['total_num'])),
                                     offset=data_config.get('offset', 0))
         S_data = full_dataset.S
