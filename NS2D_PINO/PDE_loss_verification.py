779c82f93fe2556a4f0f2c7e63c59daa645c8fbc
xuesongwang <xuesong.wang1@unsw.edu.au>
2026-02-24 12:23:54 +1100

    Refactor `PDE_loss_verification.py` to improve data handling and loss verification
    - Introduced a new function `load_ns_ground_truth` for loading and reshaping the dataset, enhancing clarity and modularity.
    - Updated `verify_pde_loss` to accommodate the new data structure and removed unused parameters, streamlining the loss computation process.
    - Adjusted the main execution block to utilize the new data loading function and updated the forcing function to `get_forcing_vel` for consistency with the new data format.

--- NS2D_PINO/PDE_loss_verification.py
+++ NS2D_PINO/PDE_loss_verification.py
@@ -5,23 +5,40 @@ import yaml
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from data_utils.datasets import NSLoader2D
-from utils.criterion import LpLoss, PINO_loss3d, get_forcing
-from argparse import ArgumentParser
+from utils.criterion import LpLoss, PINO_loss3d_vel, get_forcing_vel
+# from argparse import ArgumentParser
 import torch
+import numpy as np
 
 
-def verify_pde_loss(data, v, t_duration, forcing, device):
+def load_ns_ground_truth(datapath, nt=63, device=None):
+    data= np.load(os.path.join(datapath, 'kolmogorov_dataset.npz'))
+    X_train, y_train = data['X_train'], data['y_train']
+    nc, nx, ny = X_train.shape[1:]
+    X_train = X_train.reshape(-1, nt, nc, nx, ny)
+    y_train = y_train.reshape(-1, nt, nc, nx, ny)
+    
+    u = np.concatenate([X_train[:, :1], y_train], axis=1) # (N, nt+1, nc, nx, ny) concatenate the initial condition and the trajectory
+    u = torch.from_numpy(u).to(device)
+    return u, nx
+
+
+
+def verify_pde_loss(data, forcing, device):
     """ 
-    data: (N, S, S, T) from NSLoader2D.data, compute PINO_loss to test if the PDE loss is zero
+    data: (N, nt+1, 3, nx, ny) from load_ns_ground_truth, compute PINO_loss to test if the PDE loss is zero
+    # u: Predicted fields, shape (batchsize, 3, nx, ny, nt).
+    # Channels: ux, uy, p.
+    # u0: Initial condition, shape (batchsize, 3, nx, ny) or broadcastable.
     """
     total_loss_f = 0.0
     total_loss_ic = 0.0
+    data = data.permute(0, 2, 3, 4, 1) # (N, nt+1, 3, nx, ny) -> (N, 3, nx, ny, nt+1)
     for i in range(len(data)):
         print(f'Sample {i} shape: {data[i].shape}')
-        x = data[i].to(device).unsqueeze(0) # one realization (1, S, S, T)
-        u0 = x[..., 0] # initial condition (1, S, S)
-        loss_ic, loss_f = PINO_loss3d(x, u0, forcing, v, t_duration)
+        u = data[i].unsqueeze(0) # one realization (1, 3, nx, ny, nt+1)
+        u0 = u[..., 0] # initial condition (1, 3, nx, ny)
+        loss_ic, loss_f = PINO_loss3d_vel(u, u0, forcing)
         print(f'Sample {i} PDE loss: {loss_f.item()}, IC loss: {loss_ic.item()}')
         total_loss_f += loss_f.item()
         total_loss_ic += loss_ic.item()
@@ -32,35 +49,11 @@ def verify_pde_loss(data, v, t_duration, forcing, device):
 
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-    # parse options
-    parser = ArgumentParser(description='Basic paser')
-    parser.add_argument('--config_path', type=str, help='Path to the configuration file')
-    args = parser.parse_args()
-
-    config_file = args.config_path
-    with open(config_file, 'r') as stream:
-        config = yaml.load(stream, yaml.FullLoader)
-
-
-    data_config = config['data']
-
-    full_dataset = NSLoader2D(datapath1=data_config['datapath'],
-                                    nx=data_config['nx'], nt=data_config['nt'],
-                                    sub=data_config['sub'], sub_t=data_config['sub_t'],
-                                    N=data_config['total_num'],
-                                    t_interval=data_config['t_duration'],
-                                    n_samples=data_config.get('n_sample', data_config.get('n_samples', data_config['total_num'])),
-                                    offset=data_config.get('offset', 0))
-
-    data = full_dataset.data
-    v = 1.0 / config['data']['Re']
-    t_duration = config['data'].get('t_duration', 0.125)
-    print("t_duration: ", t_duration)
-    print("v: ", v)
-    S_forcing = data_config['nx']
-    forcing = get_forcing(S_forcing).to(device)
     
-    verify_pde_loss(data, v, t_duration, forcing, device)
+    data, S_forcing = load_ns_ground_truth(datapath='/scratch3/wan410/operator_learning_data/Dedalus/ShallowWater/kolmogorov_dataset.npz', device=device)
+    # parse options
+    forcing = get_forcing_vel(S_forcing).to(device)
+    
+    verify_pde_loss(data, forcing, device)
 
 
